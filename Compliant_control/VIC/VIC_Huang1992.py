d64495bb7a5c5c8c81faa3b37a352592809698d5
Martin <martihmy@stud.ntnu.no>
2021-02-18 20:53:12 +0100

    The VIC is now functioning as it should

--- Compliant_control/VIC/VIC_Huang1992.py
+++ Compliant_control/VIC/VIC_Huang1992.py
@@ -23,19 +23,39 @@ np.set_printoptions(precision=2)
 
 
 # is adjusted continously
-K = 50 * np.identity(6)
-B = K*0.633
-M = np.identity(6)
+Kp = 120
+Ko = 6000
+K = np.array([[Kp, 0, 0, 0, 0, 0],
+                [0, Kp, 0, 0, 0, 0],
+                [0, 0, Kp, 0, 0, 0],
+                [0, 0, 0, Ko, 0, 0],
+                [0, 0, 0, 0, Ko, 0],
+                [0, 0, 0, 0, 0, Ko]])
+
+
+Bp = Kp/10
+Bo = 1250
+B = np.array([[Bp, 0, 0, 0, 0, 0],
+                [0, Bp, 0, 0, 0, 0],
+                [0, 0, Bp, 0, 0, 0],
+                [0, 0, 0, Bo, 0, 0],
+                [0, 0, 0, 0, Bo, 0],
+                [0, 0, 0, 0, 0, Bo]])
+Mp = 10
+Mo = 10
+M_diag = np.array([Mp,Mp,Mp,Mo,Mo,Mo])
+M = np.diagflat(M_diag)
 
 #
-K_v = 0.5*np.identity(6)
+K_v = np.identity(6)
 
 P = np.identity(6)
 gamma = np.identity(18)
+gamma[2,2] = 8
 
 
 
-max_num_it = 500
+max_num_it = 2000
 
 def get_derivative_of_vector(history,iteration,T):
     size = history.shape[0]
@@ -52,15 +72,29 @@ def get_xi(goal_ori, p_d, x_d_dot, x_d_ddot, v_history, i, T):
     E_diag = np.diagflat(E)
     E_dot_diag = np.diagflat(E_dot)
     E_ddot_diag = np.diagflat(E_ddot)
-    return np.block([E_diag,E_dot_diag,E_ddot_diag])
+    return np.block([E_ddot_diag,E_dot_diag,E_diag])
+    #return np.block([E_diag,E_dot_diag,E_ddot_diag])
 
 def get_lambda_dot(gamma,xi,K_v,P,F_d):
     return np.linalg.multi_dot([-np.linalg.inv(gamma),xi.T,np.linalg.inv(K_v),P,get_F_ext(two_dim=True)-F_d.reshape([6,1])])
 
 def update_MBK_hat(lam,M,B,K):
-    return  M + np.diagflat(lam[0:6]), B + np.diagflat(lam[6:12]), K + np.diagflat(lam[12:18])
-    
 
+    M_hat = M + np.diagflat(lam[0:6])
+    B_hat = B + np.diagflat(lam[6:12])
+    K_hat = K + np.diagflat(lam[12:18])
+    ensure_limits(1,5000,M_hat)
+    ensure_limits(1,5000,B_hat)
+    ensure_limits(1,5000,K_hat)
+
+    return M_hat, B_hat, K_hat
+
+def ensure_limits(lower,upper,matrix):   
+    for i in range(6):
+        if matrix[i,i] > upper:
+            matrix[i,i] = upper
+        elif matrix[i,i] < lower:
+            matrix[i,i] = lower
 
 def get_W(inv = False):
     if inv == True:
@@ -85,7 +119,7 @@ def get_p(two_dim=False):
 def get_x_dot():
     a = np.append(robot.endpoint_velocity()['linear'],robot.endpoint_velocity()['angular'])
     b = np.zeros(6)
-    return b
+    return a
     #return np.append(robot.endpoint_velocity()['linear'],robot.endpoint_velocity()['angular'])
 
 def get_delta_x(goal_ori, p_d, two_dim = False):
@@ -114,8 +148,8 @@ def get_desired_trajectory(iterations,T):
     p = np.zeros((3,iterations))
     p[:,0] = get_p()
     if iterations > 300:
-        a[2,0:5]=-0.00002/T**2
-        a[2,295:300]=0.00002/T**2
+        a[2,0:100]=-0.00001/T**2
+        a[2,250:350]=0.00001/T**2
     if iterations > 6500:
         a[0,4500:4510]=0.00001/T**2
         a[0,6490:6500]=-0.00001/T**2
@@ -132,7 +166,7 @@ def perform_torque(M, B, K, x_d_ddot, x_d_dot, p_d, goal_ori):
     b = np.array([np.dot(M,x_d_ddot)]).reshape([6,1]) + np.array([np.dot(B,get_x_dot_delta(x_d_dot))]).reshape([6,1]) + np.array([np.dot(K,get_delta_x(goal_ori,p_d,two_dim = True))]).reshape([6,1])
     c = robot.coriolis_comp().reshape([7,1])
     d = (np.identity(6)-np.dot(get_W(inv=True),np.linalg.inv(M))).reshape([6,6])
-    total_torque = np.array([np.dot(a,b)]).reshape([7,1]) + c #+ np.array([np.linalg.multi_dot([robot.jacobian().T,d,get_F_ext()])]).reshape([7,1])
+    total_torque = np.array([np.dot(a,b)]).reshape([7,1]) + c + np.array([np.linalg.multi_dot([robot.jacobian().T,d,get_F_ext()])]).reshape([7,1])
     robot.set_joint_torques(dict(list(zip(robot.joint_names(),total_torque))))
 
 def quatdiff_in_euler(quat_curr, quat_des):
@@ -176,7 +210,7 @@ def get_F_d(max_num_it,T): #current
 
     return s
 
-def plot_result(p,p_d, delta_x, F_ext,F_d, z_stiffness, T):
+def plot_result(p,p_d, delta_x, F_ext,F_d, z_dynamics,M,B,K, T):
 
     time_array = np.arange(len(p[0]))*T
     
@@ -211,8 +245,13 @@ def plot_result(p,p_d, delta_x, F_ext,F_d, z_stiffness, T):
     plt.legend()
 
     plt.subplot(224)
-    plt.title("Stiffness along z")
-    plt.plot(time_array, z_stiffness, label = "stiffness in z")
+    plt.title("Adaptive dynamics along the z-axis")
+    plt.plot(time_array, z_dynamics[0], label = "inertia (M_z)")
+    plt.plot(time_array, z_dynamics[1], label = "damping (B_z)")
+    plt.plot(time_array, z_dynamics[2], label = "stiffness (K_z)")
+    plt.axhline(y=M[2][2], label = "initial inertia (M_z)", color='b',linestyle='dashed')
+    plt.axhline(y=B[2][2], label = "initial damping (B_z)", color='C1',linestyle='dashed')
+    plt.axhline(y=K[2][2], label = "initial stiffness (K_z)", color='g',linestyle='dashed')
     plt.xlabel("Real time [s]")
     plt.legend()
 
@@ -247,7 +286,7 @@ if __name__ == "__main__":
     F_ext_history = np.zeros((6,max_num_it))
 
 
-    z_stiffness_history = np.zeros(max_num_it)
+    z_dynamics_history = np.zeros((3,max_num_it))
 
     x_d_ddot, x_d_dot, p_d = get_desired_trajectory(max_num_it,T)
     goal_ori = np.asarray(robot.endpoint_pose()['orientation'])
@@ -269,7 +308,11 @@ if __name__ == "__main__":
 
 
         # plotting and printing
-        z_stiffness_history[i]=K_hat[2][2]
+        z_dynamics_history[0][i]=M_hat[2][2]
+        z_dynamics_history[1][i]=B_hat[2][2]
+        z_dynamics_history[2][i]=K_hat[2][2]
+
+
         if i%100 == 0:
             print(i,'/',max_num_it,' = ',T*i,' [s]   ) Force in z: ',F_ext_history[2,i])
             print(K_hat[2][2])
@@ -282,5 +325,5 @@ if __name__ == "__main__":
         #trajectory[:,i] = np.array([robot.endpoint_pose()['position'][0],robot.endpoint_pose()['position'][1],robot.endpoint_pose()['position'][2]])#
     
     #np.save('trajectory.npy',trajectory)#
-    plot_result(p_history, p_d, delta_x_history, F_ext_history, F_d, z_stiffness_history, T)
+    plot_result(p_history, p_d, delta_x_history, F_ext_history, F_d, z_dynamics_history,M,B,K, T)
 
