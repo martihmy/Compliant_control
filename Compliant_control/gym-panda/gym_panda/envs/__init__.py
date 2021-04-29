a1b34e97d4cb42878c8074d4fe82101924c390fe
Martin <martihmy@stud.ntnu.no>
2021-04-29 10:10:15 +0200

    upport for running a normalised HMFC-env

--- Compliant_control/gym-panda/gym_panda/envs/__init__.py
+++ Compliant_control/gym-panda/gym_panda/envs/__init__.py
@@ -2,5 +2,5 @@ from gym_panda.envs.panda_env_admittance import AdmittanceEnv
 from gym_panda.envs.panda_env_admittance import Normalised_Env
 
 from gym_panda.envs.HMFC_Env import HMFC_Env
-
+from gym_panda.envs.HMFC_Env import Normalised_HMFC_Env
 
