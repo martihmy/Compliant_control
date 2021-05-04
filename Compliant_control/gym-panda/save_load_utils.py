1439273a475006029750b5cf1cc5cfc0fb62adf5
Martin <martihmy@stud.ntnu.no>
2021-05-04 17:17:46 +0200

    can save and load models and policies when using rbf

--- Compliant_control/gym-panda/save_load_utils.py
+++ Compliant_control/gym-panda/save_load_utils.py
@@ -7,17 +7,26 @@ from pilco.models.pilco import PILCO
 from pilco.rewards import ExponentialReward
 
 
-def save_pilco_model(pilco_object,X1,X,Y,path):
+def save_pilco_model(pilco_object,X1,X,Y,path,rbf=True):
     for i,m in enumerate(pilco_object.mgpr.models):
         tf.saved_model.save(m,path + '/model'+ str(i) )
-        #np.save(path + "model_" + str(i) + ".npy", m.read_values())
-    #tf.saved_model.save(model,path)
+
+    if rbf:
+        for i,m in enumerate(pilco_object.controller.models):
+            tf.saved_model.save(m,path + '/control_model'+ str(i) )
+    else:
+        print('W = ',pilco_object.controller.W)
+        print('b = ',pilco_object.controller.B)
+        #np.savetxt(path + '/controller_W.csv', pilco_object.controller.W, delimiter=',')
+        #np.savetxt(path + '/controller_b.csv', pilco_object.controller.b, delimiter=',')
+
+
     np.savetxt(path + '/X1.csv', X1, delimiter=',')
     np.savetxt(path + '/X.csv', X, delimiter=',')
     np.savetxt(path + '/Y.csv', Y, delimiter=',')
 
 
-def load_pilco_model(path,controller,horizon):
+def load_pilco_model(path,controller,horizon, F_weight, rbf=True):
     #saved_data = tf.saved_model.load(path)
     X1 = np.loadtxt(path + '/X1.csv', delimiter=',')
     X = np.loadtxt(path + '/X.csv', delimiter=',')
@@ -28,7 +37,7 @@ def load_pilco_model(path,controller,horizon):
     
     target , target[0]= np.zeros(state_dim), 3
     W_diag = np.zeros(state_dim)
-    W_diag[0] = 1
+    W_diag[0] = F_weight
 
     norm_env_m = np.mean(X1[:,:state_dim],0)
     norm_env_std = np.std(X1[:,:state_dim], 0)
@@ -41,4 +50,8 @@ def load_pilco_model(path,controller,horizon):
     for i,m in enumerate(pilco.mgpr.models):
         m = tf.saved_model.load(path + '/model'+ str(i))
     
-    return pilco,X1, m_init, S_init, state_dim
+    if rbf:
+        for i,m in enumerate(pilco.controller.models):
+            m = tf.saved_model.load(path + '/control_model'+ str(i))
+    
+    return pilco,X1, m_init, S_init, state_dim, X, Y
